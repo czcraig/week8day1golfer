103bfd96aa9712998ac0e5b74dfb8f8914541f22
Chris Craig <ctc2@live.co.uk>
2018-07-09 22:39:12 +0100

    added more golfers

--- src/main/java/models/Runner.java
+++ src/main/java/models/Runner.java
@@ -6,8 +6,18 @@ import models.Golfer;
 public class Runner {
 
     public static void main(String[] args) {
-        Golfer golfer= new Golfer("Tiger", "Woods", 39);
-        DBGolfer.save(golfer);
+        Golfer golfer1 = new Golfer("Tiger", "Woods", 39);
+        DBGolfer.save(golfer1);
+
+        Golfer golfer2 = new Golfer("Phil", "Mickelson", 49);
+        DBGolfer.save(golfer2);
+
+        Golfer golfer3 = new Golfer("Rory", "Mcilroy", 25);
+        DBGolfer.save(golfer3);
+
+        Golfer golfer4 = new Golfer("Lee", "Westwood", 30);
+        DBGolfer.save(golfer4);
     }
+
 }
 
